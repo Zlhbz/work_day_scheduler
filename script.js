e91f2d0fb1eeb90bc084bcca3ae111ccffaaf9d3
Zeliha Gencel <zelihagencel@gmail.com>
2019-12-22 21:57:56 -0600

    all needed 3 function were done

--- script.js
+++ script.js
@@ -1,39 +1,86 @@
+
+console.log(window);
 var day = $("#currentDay");
-
 day.text(moment().format("dddd, MMMM Do YYYY"));
-
-// day.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
-// day.text(moment().toDate());
-
 var data_hour = moment().format("H");
 
-console.log(moment().format("H"));
-console.log(moment().toDate());
+arrange();
+showAll()
+saveData();
 
-for (var i = 9; i < 17; i++) {
 
-    var new_div = $("<div>");
-    var time_hour = $("<div>");
+function arrange() {
+    for (var i = 9; i < 17; i++) {
+        var new_div = $("<div>");
+        new_div.attr("class", "row");
 
-    var save_button = $("<button>");
+        var time_hour = $("<div>");
+        time_hour.attr("class", "hour time-block col-2 col-sm-1 col-md-1 col-lg-1");
+        if (i < 12) {
+            time_hour.text(i + "AM");
+        }
+        if (i === 12) {
+            time_hour.text(i + "PM");
+        }
+        if (i > 12) {
+            time_hour.text(i - 12 + "PM");
+        }
 
-    new_div.attr("class", "row");
+        var input = $("<textarea>");
+        if (i < data_hour) {
+            input.attr("class", "description past col-8 col-sm-10 col-md-10 col-lg-10");
+        }
+        if (i === parseInt(data_hour)) {
+            input.attr("class", "description present col-8 col-sm-10 col-md-10 col-lg-10");
+        }
+        if (i > data_hour) {
+            input.attr("class", "description future col-8 col-sm-10 col-md-10 col-lg-10");
+        }
+        input.attr("set_text", "hour" + i);
 
-    time_hour.attr("class", "hour time-block col-1 col-sm-1 col-md-1 col-lg-1");
-    time_hour.text(i + "AM");
+        var save_button = $("<button>");
+        save_button.attr("set_button", "hour" + i);
 
-    var input = $("<textarea>");
-    if (i < data_hour) {
-        input.attr("class", "past col-10 col-sm-10 col-md-10 col-lg-10");
+        save_button.attr("class", "saveBtn col-2 col-sm-1 col-md-1 col-lg-1");
+        new_div.append(time_hour, input, save_button);
+        $(".container").append(new_div);
     }
-    if (i === parseInt(data_hour)) {
-        input.attr("class", "present col-10 col-sm-10 col-md-10 col-lg-10");
-    }
-    if (i > data_hour) {
-        input.attr("class", "future col-10 col-sm-10 col-md-10 col-lg-10");
-    }
-
-    save_button.attr("class", "saveBtn col-1 col-sm-1 col-md-1 col-lg-1");
-    new_div.append(time_hour, input, save_button);
-    $(".container").append(new_div);
 }
+
+function saveData() {
+    $(".saveBtn").on("click", function () {
+
+        var customText = $(this).attr("set_button");
+        var key_date = moment().format("MMDDYYYY") + customText;
+
+        $("textarea").each(function () {
+            if ($(this).attr("set_text") === customText) {
+                var objectAll = window.localStorage.getItem("day_schedule")
+                if (objectAll == null) {
+                    objectAll = { key_date: $(this).val() }
+                } else {
+                    objectAll = JSON.parse(objectAll);
+                    objectAll[key_date] = $(this).val();
+
+                }
+                window.localStorage.setItem("day_schedule", JSON.stringify(objectAll));
+            }
+        })
+    })
+}
+
+function showAll() {
+    var stringAll = window.localStorage.getItem("day_schedule");
+    var objectAll = JSON.parse(stringAll);
+    for (var i = 9; i < 17; i++) {
+
+        $("textarea").each(function () {
+            if ($(this).attr("set_text") === "hour" + i) {
+                console.log(objectAll[moment().format("MMDDYYYY") + "hour" + i])
+                $(this).val(objectAll[moment().format("MMDDYYYY") + "hour" + i]);
+            }
+        })
+    }
+}
+
+
